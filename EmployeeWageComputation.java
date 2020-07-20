09761ae5dbe9586703dd9bc7113b1c27a5122c9c
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-20 14:26:04 +0530

    [Prasanth] Calculated Wage for Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,15 +6,16 @@ public class EmployeeWageComputation{
 			int WAGE_PER_HOUR = 20;
 			int FULL_DAY_HOUR = 8;
 			int PART_TIME_HOUR = 8;
+			int WORKING_DAY_PER_MONTH = 20;
 			Random random = new Random();
 			int attendance = random.nextInt(3);  
 			switch (attendance) {
 			case 1 :
-				System.out.println("Full Daily Wage is : " + WAGE_PER_HOUR*FULL_DAY_HOUR);
+				System.out.println("Full Monthly Wage is : " + WORKING_DAY_PER_MONTH*WAGE_PER_HOUR*FULL_DAY_HOUR);
 				System.out.println("Employee is Present");  
 				break;
 			case 2 :
-				System.out.println("Part Time Daily Wage is : " + WAGE_PER_HOUR*PART_TIME_HOUR);
+				System.out.println("Part Time Monthly Wage is : " + WAGE_PER_HOUR*WORKING_DAY_PER_MONTH*PART_TIME_HOUR);
             System.out.println("Employee is Present");  
 				break;
 			default :
