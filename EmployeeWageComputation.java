cec1e7ccb9fde7ef5990c6e3fbd5d6f53ef9692a
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-20 14:07:17 +0530

    [Prasanth] Daily Wage Calculated

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,11 +3,14 @@ import java.util.Random;
 public class EmployeeWageComputation{
 
 	public static void main(String[] args){
+			int WAGE_PER_HOUR = 20;
+			int FULL_DAY_HOUR = 8;
 			Random random = new Random();
-			int attendance = random.nextInt(2);
-			if (attendance == 1 )
-				System.out.println("Present");
+			int attendance = random.nextInt(2);  
+			if (attendance == 1 ) 	{
+				System.out.println("Daily Wage is : " + WAGE_PER_HOUR*FULL_DAY_HOUR);
+				System.out.println("Employee is Present");  }
 			else
-				System.out.println("Absent");
+				System.out.println("Employee is Absent");
 	}
 }
