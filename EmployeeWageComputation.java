33b12625d2c46fb0a0d8b8730ec4777da45f3e23
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-20 14:13:47 +0530

    [Prasanth] Dealt with PartTime Emp and Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,11 +5,15 @@ public class EmployeeWageComputation{
 	public static void main(String[] args){
 			int WAGE_PER_HOUR = 20;
 			int FULL_DAY_HOUR = 8;
+			int PART_TIME_HOUR = 8;
 			Random random = new Random();
-			int attendance = random.nextInt(2);  
+			int attendance = random.nextInt(3);  
 			if (attendance == 1 ) 	{
-				System.out.println("Daily Wage is : " + WAGE_PER_HOUR*FULL_DAY_HOUR);
+				System.out.println("Full Daily Wage is : " + WAGE_PER_HOUR*FULL_DAY_HOUR);
 				System.out.println("Employee is Present");  }
+			else if(attendance == 2 ){
+				System.out.println("Part Time Daily Wage is : " + WAGE_PER_HOUR*PART_TIME_HOUR);
+            System.out.println("Employee is Present");  }
 			else
 				System.out.println("Employee is Absent");
 	}
