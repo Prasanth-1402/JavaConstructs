6064269f16b9c000d1b319076523e5bb473e95e7
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-20 13:53:56 +0530

    [Prasanth] Took Attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,1 +1,13 @@
+import java.util.Random;
 
+public class EmployeeWageComputation{
+
+	public static void main(String[] args){
+			Random random = new Random();
+			int attendance = random.nextInt(2);
+			if (attendance == 1 )
+				System.out.println("Present");
+			else
+				System.out.println("Absent");
+	}
+}
