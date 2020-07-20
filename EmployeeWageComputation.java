30f1824de61f949fc8856d48177f558d8aad595a
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-20 14:21:02 +0530

    [Prasanth] Done the same with Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,13 +8,17 @@ public class EmployeeWageComputation{
 			int PART_TIME_HOUR = 8;
 			Random random = new Random();
 			int attendance = random.nextInt(3);  
-			if (attendance == 1 ) 	{
+			switch (attendance) {
+			case 1 :
 				System.out.println("Full Daily Wage is : " + WAGE_PER_HOUR*FULL_DAY_HOUR);
-				System.out.println("Employee is Present");  }
-			else if(attendance == 2 ){
+				System.out.println("Employee is Present");  
+				break;
+			case 2 :
 				System.out.println("Part Time Daily Wage is : " + WAGE_PER_HOUR*PART_TIME_HOUR);
-            System.out.println("Employee is Present");  }
-			else
+            System.out.println("Employee is Present");  
+				break;
+			default :
 				System.out.println("Employee is Absent");
+			}
 	}
 }
