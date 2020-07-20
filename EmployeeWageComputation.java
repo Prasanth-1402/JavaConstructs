d6de2da6c620c2e7d1d633e1751f5c0a04f41ecd
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-20 15:07:34 +0530

    [Prasanth] Calculated Wages till 100hrs or 20Days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,18 +5,29 @@ public class EmployeeWageComputation{
 	public static void main(String[] args){
 			int WAGE_PER_HOUR = 20;
 			int FULL_DAY_HOUR = 8;
-			int PART_TIME_HOUR = 8;
+			int PART_TIME_HOUR = 4;
 			int WORKING_DAY_PER_MONTH = 20;
+			int HourCount=FULL_DAY_HOUR;
+         int DayCount=1;
+         int Payment=0;
 			Random random = new Random();
 			int attendance = random.nextInt(3);  
 			switch (attendance) {
 			case 1 :
-				System.out.println("Full Monthly Wage is : " + WORKING_DAY_PER_MONTH*WAGE_PER_HOUR*FULL_DAY_HOUR);
-				System.out.println("Employee is Present");  
+				while(HourCount < 101 && DayCount < WORKING_DAY_PER_MONTH){
+					Payment = WAGE_PER_HOUR*FULL_DAY_HOUR + Payment;
+					DayCount++;
+					HourCount+=8;	
+				}
+				System.out.println("FINAL PAY FOR FULL TIME EMPLOYEE :" +Payment);
 				break;
 			case 2 :
-				System.out.println("Part Time Monthly Wage is : " + WAGE_PER_HOUR*WORKING_DAY_PER_MONTH*PART_TIME_HOUR);
-            System.out.println("Employee is Present");  
+            while(HourCount < 101 && DayCount < WORKING_DAY_PER_MONTH){
+               Payment = WAGE_PER_HOUR*PART_TIME_HOUR + Payment;
+               DayCount++;
+               HourCount+=4;
+            }
+            System.out.println("FINAL PAY FOR PART TIME EMPLOYEE :" +Payment);
 				break;
 			default :
 				System.out.println("Employee is Absent");
