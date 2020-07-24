61333a1a82e23951065ab2a336758b7e71833889
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-24 22:43:03 +0530

    [Prasanth] Refactored code to include Method with Parameters

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,36 +1,66 @@
 import java.util.Random;
-
+import java.util.*;
 public class EmployeeWageComputation{
 
-	public static void main(String[] args){
-			int WAGE_PER_HOUR = 20;
-			int FULL_DAY_HOUR = 8;
-			int PART_TIME_HOUR = 4;
-			int WORKING_DAY_PER_MONTH = 20;
-			int HourCount=FULL_DAY_HOUR;
-         int DayCount=1;
-         int Payment=0;
-			Random random = new Random();
-			int attendance = random.nextInt(3);  
-			switch (attendance) {
-			case 1 :
-				while(HourCount < 101 && DayCount < WORKING_DAY_PER_MONTH){
-					Payment = WAGE_PER_HOUR*FULL_DAY_HOUR + Payment;
-					DayCount++;
-					HourCount+=8;	
-				}
-				System.out.println("FINAL PAY FOR FULL TIME EMPLOYEE :" +Payment);
-				break;
-			case 2 :
-            while(HourCount < 101 && DayCount < WORKING_DAY_PER_MONTH){
-               Payment = WAGE_PER_HOUR*PART_TIME_HOUR + Payment;
-               DayCount++;
-               HourCount+=4;
+    public static void main(String[] args){
+
+         System.out.println("Enter the number of companies:");
+         Scanner scan = new Scanner(System.in);
+         int companyCount=scan.nextInt();
+         scan.nextLine();
+
+         for(int i=1;i<=companyCount;i++){
+
+         System.out.println("Enter the Company Name :");
+         String companyName = scan.nextLine();
+
+         System.out.println("Enter wage per hour for the company "+companyName+" :");
+
+         int WAGE_PER_HOUR=scan.nextInt();
+
+         System.out.println("Enter Full day hour");
+         int FULL_DAY_HOUR=scan.nextInt();
+
+         System.out.println("Enter part time hour ");
+         int PART_TIME_HOUR=scan.nextInt();
+
+         System.out.println("enter working day per month ");
+         int WORKING_DAY_PER_MONTH=scan.nextInt();
+         scan.nextLine();
+
+            checkEmployeePayment(companyName,WAGE_PER_HOUR,FULL_DAY_HOUR,PART_TIME_HOUR,WORKING_DAY_PER_MONTH);
+         }
+    }
+
+
+    public static void checkEmployeePayment(String CompanyName, int WAGE_PER_HOUR,int FULL_DAY_HOUR,int PART_TIME_HOUR,int WORKING_DAY_PER_MONTH ){
+        System.out.println("The Company Name is :"+CompanyName);
+        int hourCount=FULL_DAY_HOUR;
+        int dayCount=1;
+        int payment=0;
+        for(int counter=0;counter<WORKING_DAY_PER_MONTH;counter++) {
+            int attendance = takeAttendance();
+            switch (attendance){
+                case 1 :
+                    payment = WAGE_PER_HOUR * FULL_DAY_HOUR + payment;
+                    break;
+                case 2 :
+                    payment = WAGE_PER_HOUR * PART_TIME_HOUR + payment;
+                    break;
+                default:
+                    payment=payment+0;
+                    break;
             }
-            System.out.println("FINAL PAY FOR PART TIME EMPLOYEE :" +Payment);
-				break;
-			default :
-				System.out.println("Employee is Absent");
-			}
-	}
+        }
+        System.out.println("The Payment from "+CompanyName+" is : "+payment);
+    }
+
+
+    public static int takeAttendance(){
+        Random random = new Random();
+        int attendance = random.nextInt(3);
+        return attendance;
+    }
 }
+
+
